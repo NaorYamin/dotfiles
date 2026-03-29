9ea04c451a1a7f939d2ad94da65513fe78a2dccf
NaorYamin <naory902@gmail.com>
2026-03-29 20:05:21 +0300

    added keymaps.lua | It's a keyboard shortcurs

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Start selecting and move right (Normal mode)
+vim.keymap.set("n", "<C-S-Right>", "v<Right>", { desc = "Select right" })
+-- Start selecting and move left (Normal mode)
+vim.keymap.set("n", "<C-S-Left>", "v<Left>", { desc = "Select left" })
+
+-- Continue selecting right/left while already in Visual mode
+vim.keymap.set("v", "<C-S-Right>", "<Right>", { desc = "Extend selection right" })
+vim.keymap.set("v", "<C-S-Left>", "<Left>", { desc = "Extend selection left" })
+
+-- Selection in Insert mode (Goes to visual mode and starts selecting)
+vim.keymap.set("i", "<C-S-Right>", "<Esc>lv<Right>", { desc = "Select right from insert" })
+vim.keymap.set("i", "<C-S-Left>", "<Esc>v<Left>", { desc = "Select left from insert" })
